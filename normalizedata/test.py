23a46831b8d4544b5cff3178c01e49094d44e493
Misagh <Mustafakhatami@gmail.com>
2022-11-19 19:08:21 +0330

    Add peak and valley point

--- normalizedata/test.py
+++ normalizedata/test.py
@@ -3,28 +3,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.signal import argrelextrema
 
-# Generate a noisy AR(1) sample
 
-np.random.seed(0)
-rs = np.random.randn(200)
-xs = [0]
-for r in rs:
-    xs.append(xs[-1] * 0.9 + r)
-df = pd.DataFrame(xs, columns=['data'])
-
-n = 5  # number of points to be checked before and after
+#df = pd.DataFrame(xs, columns=['data'])
+df = pd.read_csv('D:/BTC-USDT.csv')
+n = 10  # number of points to be checked before and after
 
 # Find local peaks
 
-df['min'] = df.iloc[argrelextrema(df.data.values, np.less_equal,
-                    order=n)[0]]['data']
-df['max'] = df.iloc[argrelextrema(df.data.values, np.greater_equal,
-                    order=n)[0]]['data']
+df['min'] = df.iloc[argrelextrema(df.LOW.values, np.less_equal,
+                    order=n)[0]]['LOW']
+df['max'] = df.iloc[argrelextrema(df.HIGH.values, np.greater_equal,
+                    order=n)[0]]['HIGH']
 
 # Plot results
 
 plt.scatter(df.index, df['min'], c='r')
 plt.scatter(df.index, df['max'], c='g')
-plt.plot(df.index, df['data'])
+plt.plot(df.index, df['CLOSE'])
 plt.show()
 print(df['min'])
